b5554d055cf2277a91e9c5a10184e3f9ef701118
paulalvarado <paulperezsv@gmail.com>
2023-10-30 08:15:20 -0600

    first change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './components/boton/Boton';
+import BotonClear from './components/botonClear/BotonClear';
+import Pantalla from './components/pantalla/Pantalla';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  const agregarInput = (val) => {
+    if (resultado) {
+      // Si hay un resultado, comenzar una nueva operación con el resultado como entrada.
+      setInput(resultado + val);
+      setResultado('');
+    } else {
+      setInput((prevInput) => prevInput + val);
+    }
+  };
+
+
+  const valoresInvalidos = ['*', '.', '/', '+', '-', '0'];
+
+  const calcularResultado = () => {
+    if (!input || valoresInvalidos.includes(input)) {
+      alert('Ingresa un valor válido');
+    } else {
+      try {
+        const result = evaluate(input);
+        setResultado(result);
+        setInput(result.toString());
+      } catch (error) {
+        alert('Error en la expresión');
+      }
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h1>Calculadora con React</h1>
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/boton/Boton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Boton.css";
+
+function Boton(props) {
+
+    const esOperador = val => {
+        return isNaN(val) && (val != ".") && (val != "=");
+    }
+
+    return (
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()} onClick={() => props.manejarClic(props.children)}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Boton;
